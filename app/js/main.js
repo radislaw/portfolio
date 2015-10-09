54af245423055da6f21c76b4229df24d53593569
Radislaw <radislaw22@gmail.com>
2015-10-09 21:39:27 +1000

    Fix file upload field

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,45 @@
 // BPOPUP
 // Semicolon (;) to ensure closing of earlier scripting
-    // Encapsulation
-    // $ is assigned to jQuery
-    ;(function($) {
+// Encapsulation
+// $ is assigned to jQuery
+;
+(function ($) {
 
-         // DOM Ready
-        $(function() {
+    // DOM Ready
+    $(function () {
 
-            // Binding a click event
-            // From jQuery v.1.7.0 use .on() instead of .bind()
-            $('#pop-up').bind('click', function(e) {
+        // Binding a click event
+        // From jQuery v.1.7.0 use .on() instead of .bind()
+        $('#pop-up').bind('click', function (e) {
 
-                // Prevents the default action to be triggered.
-                e.preventDefault();
+            // Prevents the default action to be triggered.
+            e.preventDefault();
 
-                // Triggering bPopup when click event is fired
-                $('#element_to_pop_up').bPopup();
-
-            });
+            // Triggering bPopup when click event is fired
+            $('#element_to_pop_up').bPopup();
 
         });
 
-    })(jQuery);
+    });
+
+})(jQuery);
+
+$('.add-file').on('change', function () {
+
+    var
+        $this = $(this),
+        value = $this.val(),
+        pureVal = value.replace(/c:\\fakepath\\/gmi, "");
+
+    $('.upload-text').text(pureVal);
+
+});
 
 // PLACEHOLDER
 
 $(document).ready(function () {
 
-    if(!Modernizr.input.placeholder){
+    if (!Modernizr.input.placeholder) {
 
         $('input, textarea').placeholder();
 
