1eaaf72121344d45938bb6851ef644d702ea0d1a
Radislaw <radislaw22@gmail.com>
2015-10-14 17:03:23 +1000

    add validation via qtip for add-project popup form

---
+++ app/js/add-project.js
@@ -0,0 +1,28 @@
+var touch = (function () {
+
+    var init = function () {
+            _setUpListeners();
+
+        },
+        _setUpListeners = function () {
+            $('#add-new-project').on('submit', _submitForm);
+        };
+
+    var _submitForm = function (e) {
+        e.preventDefault();
+
+        var form = $(this),
+            url = 'contact-me.php',
+            defObj = _ajaxForm(form, url);
+    };
+
+    var _ajaxForm = function (form, url) {
+        if (!validation.validateForm(form)) return false;
+    };
+    return {
+        init: init
+    };
+
+})();
+
+touch.init();
